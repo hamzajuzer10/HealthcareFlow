c0a8e7563aaa976e903dcfcac166ee0485fa33b2
Juzer <hamzajuzer@deloitte.co.uk>
2020-06-06 15:49:12 +0100

    Completed data creation

---
+++ pathway_los_prediction/create_input_dataset.py
@@ -0,0 +1,177 @@
+import pandas as pd
+import json
+import os
+from pathway_los_prediction import model_configuration
+from collections import Counter
+
+
+def load_test_data(path):
+    df = pd.read_pickle(path)
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
+
+def create_input_files(input_file_path, output_file_dir, max_pathway_len, min_ward_freq, config_dict: dict,
+                       train_pr=0.8, val_pr=0.1, test_pr=0.1):
+    """
+        Creates input files for training, validation, and test data.
+
+        :param input_file_path: name of input file path
+        :param min_ward_freq: wards occuring less frequently than this threshold are binned as <unk>s
+        :param output_file_dir: name of output file path
+        :param max_pathway_len: don't sample pathways longer than this length
+        :param config_dict: pathway_los configuration dict
+        :param train_pr: train dataset proportion
+        :param val_pr: val dataset proportion
+        :param test_pr: test dataset proportion
+        """
+
+    data_df = load_test_data(input_file_path)
+
+    # only take the cols containing the relevant features and target data
+    data_df = data_df[
+        config_dict['pathway_los_continuous_features'] + config_dict['pathway_los_categorical_features'] + config_dict[
+            'pathway_los_target']]
+
+    # get the number of wards in the pathway
+    data_df['pathway_len'] = data_df.apply(lambda x: len(x['patient_pathway']), axis=1)
+
+    # remove all data samples where pathway length is larger than max_pathway_len
+    data_df = data_df[data_df['pathway_len'] <= max_pathway_len]
+
+    # get a ward count for all wards in the pathways (based on full dataset)
+    wards_list = data_df['patient_pathway'].tolist()
+    wards_list = [y for x in wards_list for y in x]
+    ward_freq = Counter(wards_list)
+
+    # Create ward map
+    wards = [w for w in ward_freq.keys() if ward_freq[w] > min_ward_freq]
+    ward_map = {k: v + 1 for v, k in enumerate(wards)}
+    ward_map['<unk>'] = len(ward_map) + 1
+    ward_map['<start>'] = len(ward_map) + 1
+    ward_map['<end>'] = len(ward_map) + 1
+    ward_map['<pad>'] = 0
+
+    def encode_ward(x):
+        ward_list = x['patient_pathway']
+        enc_p = [ward_map['<start>']] + [ward_map.get(ward, ward_map['<unk>']) for ward in ward_list] + \
+                [ward_map['<end>']] + [ward_map['<pad>']] * (max_pathway_len - len(ward_list))
+
+        return enc_p
+
+    # Encode wards
+    data_df['patient_pathway'] = data_df.apply(encode_ward, axis=1)
+
+    # update pathway length to include both start and end codes
+    data_df['pathway_len'] += 2
+
+    # split data into train, val and test
+    train_pr = train_pr / (train_pr + val_pr + test_pr)
+    val_pr = val_pr / (train_pr + val_pr + test_pr)
+    test_pr = test_pr / (train_pr + val_pr + test_pr)
+    train_df = data_df.sample(frac=train_pr, random_state=200)  # random state is a seed value
+    _df = data_df.drop(train_df.index)
+    val_df = _df.sample(frac=val_pr / (val_pr + test_pr), random_state=200)  # random state is a seed value
+    test_df = _df.drop(val_df.index)
+
+    # reset index
+    train_df.reset_index(drop=True, inplace=True)
+    val_df.reset_index(drop=True, inplace=True)
+    test_df.reset_index(drop=True, inplace=True)
+
+    # Create a feature map for all categorical features (based on train dataset)
+    feature_map = {}
+    for col in train_df[config_dict['pathway_los_categorical_features']]:
+        v_unique = train_df[col].unique()
+        map_name = col + "_map"
+        _feature_map = {k: v + 1 for v, k in enumerate(v_unique)}
+        feature_map[map_name] = _feature_map
+
+    # Create a max min map on continuous variables
+    if config_dict['minmax_scale_continuous_features']:
+        for col in train_df[config_dict['pathway_los_continuous_features']]:
+            _max = train_df[col].max()
+            _min = train_df[col].min()
+            interval = _max - _min
+            map_name = col + "_map"
+            _feature_map = {'max': _max, 'min': _min, 'interval': interval}
+            feature_map[map_name] = _feature_map
+
+    # apply categorical feature maps to train, val and test datasets
+    for col in train_df[config_dict['pathway_los_categorical_features']]:
+        train_df[col] = train_df[col].map(feature_map[str(col) + "_map"])
+    train_df[col].fillna(value=0, inplace=True)
+
+    for col in val_df[config_dict['pathway_los_categorical_features']]:
+        val_df[col] = val_df[col].map(feature_map[str(col) + "_map"])
+    val_df[col].fillna(value=0, inplace=True)
+
+    for col in test_df[config_dict['pathway_los_categorical_features']]:
+        test_df[col] = test_df[col].map(feature_map[str(col) + "_map"])
+    test_df[col].fillna(value=0, inplace=True)
+
+    # apply continuous feature rescaling to train, val and test datasets
+    for col in train_df[config_dict['pathway_los_continuous_features']]:
+        train_df[col] = train_df.apply(lambda x: max(0, min((x[col] - feature_map[str(col) + "_map"]['min'])/feature_map[str(col) + "_map"]['interval'], 1)), axis=1)
+
+    for col in val_df[config_dict['pathway_los_continuous_features']]:
+        val_df[col] = val_df.apply(lambda x: max(0, min((x[col] - feature_map[str(col) + "_map"]['min'])/feature_map[str(col) + "_map"]['interval'], 1)), axis=1)
+
+    for col in test_df[config_dict['pathway_los_continuous_features']]:
+        test_df[col] = test_df.apply(lambda x: max(0, min((x[col] - feature_map[str(col) + "_map"]['min'])/feature_map[str(col) + "_map"]['interval'], 1)), axis=1)
+
+    # Save ward map, feature maps to a json
+    with open(os.path.join(output_file_dir, 'ward_map' + '.json'), 'w') as j:
+        json.dump(ward_map, j)
+
+    with open(os.path.join(output_file_dir, 'feature_map' + '.json'), 'w') as j:
+        json.dump(feature_map, j)
+
+    # Save encoded pathways and their lengths to JSON files
+    with open(os.path.join(output_file_dir, 'TRAIN' + '_PATHWAYS' + '.json'), 'w') as j:
+        json.dump(train_df['patient_pathway'].tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'VAL' + '_PATHWAYS' + '.json'), 'w') as j:
+        json.dump(val_df['patient_pathway'].tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'TEST' + '_PATHWAYS' + '.json'), 'w') as j:
+        json.dump(test_df['patient_pathway'].tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'TRAIN' + '_PATHWAY_LEN' + '.json'), 'w') as j:
+        json.dump(train_df['pathway_len'].tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'VAL' + '_PATHWAY_LEN' + '.json'), 'w') as j:
+        json.dump(val_df['pathway_len'].tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'TEST' + '_PATHWAY_LEN' + '.json'), 'w') as j:
+        json.dump(test_df['pathway_len'].tolist(), j)
+
+    # Save continuous features to a json
+    with open(os.path.join(output_file_dir, 'TRAIN' + '_CONT_FEATURES' + '.json'), 'w') as j:
+        json.dump(train_df[config_dict['pathway_los_continuous_features']].values.tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'VAL' + '_CONT_FEATURES' + '.json'), 'w') as j:
+        json.dump(val_df[config_dict['pathway_los_continuous_features']].values.tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'TEST' + '_CONT_FEATURES' + '.json'), 'w') as j:
+        json.dump(test_df[config_dict['pathway_los_continuous_features']].values.tolist(), j)
+
+    # Save categorical features to a json
+    with open(os.path.join(output_file_dir, 'TRAIN' + '_CAT_FEATURES' + '.json'), 'w') as j:
+        json.dump(train_df[config_dict['pathway_los_categorical_features']].values.tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'VAL' + '_CAT_FEATURES' + '.json'), 'w') as j:
+        json.dump(val_df[config_dict['pathway_los_categorical_features']].values.tolist(), j)
+
+    with open(os.path.join(output_file_dir, 'TEST' + '_CAT_FEATURES' + '.json'), 'w') as j:
+        json.dump(test_df[config_dict['pathway_los_categorical_features']].values.tolist(), j)
+
+
+if __name__ == "__main__":
+
+    create_input_files(input_file_path='..\\sample_datasets\\sample_patient_pathway_los_data.pkl',
+                       output_file_dir='..\\sample_datasets',
+                       max_pathway_len=6,
+                       min_ward_freq=20,
+                       config_dict=model_configuration.generate_pathway_los_config())
---
+++ pathway_prediction_dataset/test_data.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+import random as random
+from simulation_dataset import los_distribution
+
+
+def generate_patient_pathway_los(patient_groups, patient_group_characteristics, patient_pathways, ward_los,
+                                 num_samples, num_steps, filename):
+
+    patient_id = 1
+    gen_steps = 0
+
+    # create a final dataframe object
+    results_df = pd.DataFrame()
+
+    while patient_id < num_samples:
+
+        # sample from patient group list
+        patient_group = random.choice(patient_groups)
+
+        # append condition
+        condition = patient_group_characteristics[patient_group_characteristics['patient_group'] == patient_group].iloc[0]['condition']
+
+        # append age
+        age = round(max(18, min(np.random.normal(50, 10, 1)[0], 90)),0)
+
+        # append pathways
+        patient_pathway = patient_pathways[patient_pathways['patient_group'] == patient_group]
+        pathway = patient_pathway.sample(weights=patient_pathway.pathway_probability).iloc[0]['pathway'].copy()
+
+        # append LoS per pathway
+        los = []
+        for ward in pathway:
+
+            # calculate LoS for ward
+            ward_los_ = ward_los[ward_los['patient_group'] == patient_group]
+            los_distribution = ward_los_[ward_los_['ward_name'] == ward].iloc[0]['los_probability']
+            w_los = los_distribution.sample()
+            los.append(round(w_los,0))
+
+        # append patient characteristics
+
+        # create a dataframe
+        data = {'patient_id': [patient_id],
+                'patient_condition': [condition],
+                'patient_age': [age],
+                'patient_point_of_entry': [pathway[0]],
+                'patient_pathway': [pathway[1:]],
+                'ward_los': [los[1:]]}
+        data_df = pd.DataFrame(data,
+                               columns=['patient_id', 'patient_condition', 'patient_age', 'patient_point_of_entry',
+                                        'patient_pathway', 'ward_los'])
+
+        # use an exponential process to generate timestamp data
+        patient_id += 1
+
+        # concat with results_df
+        results_df = pd.concat([results_df, data_df], axis=0)
+
+        if patient_id % num_steps == 0:
+
+            print('Completed generating patients {a} to {b}'.format(a=(gen_steps*num_steps)+1,
+                                                                    b=(gen_steps+1)*num_steps))
+            gen_steps += 1
+
+    save_path = filename + ".pkl"
+
+    save_test_data(results_df, save_path)
+
+
+def generate_test_data(filename, num_patients=100, num_steps=5000):
+
+    # create test datasets to feed into the pathway and Los prediction
+    patient_group_data = {'patient_group':  ['A', 'B', 'C', 'D'],
+                          'condition': ['Diabetes', 'Heart', 'Kidney', 'Lungs']}
+
+    patient_group_df = pd.DataFrame(patient_group_data, columns=['patient_group', 'condition'])
+
+    pathway_data = {'patient_group': ['A', 'A', 'A', 'A', 'B', 'B', 'B', 'B', 'B', 'C', 'C', 'C', 'D', 'D', 'D', 'D'],
+                    'pathway': [['A_and_E', 'Ward_1', 'Ward_3', 'Ward_4'],
+                                ['A_and_E', 'Ward_1', 'Ward_2', 'Ward_6'],
+                                ['A_and_E', 'Ward_1', 'Ward_4'],
+                                ['Elective', 'Ward_4', 'Ward_6'],
+                                ['Elective', 'Ward_4'],
+                                ['Elective', 'Ward_4', 'Ward_6'],
+                                ['Elective', 'Ward_6'],
+                                ['Elective', 'Ward_2', 'Ward_6'],
+                                ['Elective', 'Ward_2', 'Ward_3'],
+                                ['A_and_E', 'Ward_1'],
+                                ['Elective', 'Ward_3', 'Ward_4'],
+                                ['A_and_E', 'Ward_5'],
+                                ['A_and_E', 'Ward_5', 'Ward_6'],
+                                ['A_and_E', 'Ward_2', 'Ward_6'],
+                                ['A_and_E', 'Ward_2', 'Ward_4'],
+                                ['A_and_E', 'Ward_1', 'Ward_4']],
+                    'pathway_probability': [0.3, 0.3, 0.2, 0.2, 0.5, 0.1, 0.1, 0.25, 0.05, 0.3, 0.5, 0.2, 0.9, 0.05, 0.025, 0.025]}
+
+    pathway_df = pd.DataFrame(pathway_data, columns=['patient_group', 'pathway', 'pathway_probability'])
+
+    los_data = {'patient_group': ['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A',
+                                  'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B', 'B',
+                                  'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C',
+                                  'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D', 'D'],
+                'ward_name': ['A_and_E', 'Elective', 'Ward_1', 'Ward_2', 'Ward_3', 'Ward_4', 'Ward_5', 'Ward_6', 'Sink_Ward',
+                              'A_and_E', 'Elective', 'Ward_1', 'Ward_2', 'Ward_3', 'Ward_4', 'Ward_5', 'Ward_6', 'Sink_Ward',
+                              'A_and_E', 'Elective', 'Ward_1', 'Ward_2', 'Ward_3', 'Ward_4', 'Ward_5', 'Ward_6', 'Sink_Ward',
+                              'A_and_E', 'Elective', 'Ward_1', 'Ward_2', 'Ward_3', 'Ward_4', 'Ward_5', 'Ward_6', 'Sink_Ward'],
+                'los_probability': [los_distribution.LoSDistribution(120, 120, 0, 20000), los_distribution.LoSDistribution(45, 30, 0, 20000), los_distribution.LoSDistribution(1300, 1000, 0, 20000),
+                                    los_distribution.LoSDistribution(5000, 1500, 0, 20000), los_distribution.LoSDistribution(3500, 850, 0, 20000), los_distribution.LoSDistribution(1250, 250, 0, 20000),
+                                    los_distribution.LoSDistribution(10000, 2500, 0, 20000), los_distribution.LoSDistribution(8000, 2000, 0, 20000), los_distribution.LoSDistribution(1500, 150, 0, 20000),
+                                    los_distribution.LoSDistribution(120, 120, 0, 20000), los_distribution.LoSDistribution(45, 30, 0, 20000),
+                                    los_distribution.LoSDistribution(6000, 3000, 0, 20000), los_distribution.LoSDistribution(3500, 300, 0, 20000), los_distribution.LoSDistribution(4500, 1000, 0, 20000),
+                                    los_distribution.LoSDistribution(12000, 2000, 0, 20000), los_distribution.LoSDistribution(5600, 450, 0, 20000), los_distribution.LoSDistribution(8000, 300, 0, 20000),
+                                    los_distribution.LoSDistribution(7500, 1300, 0, 20000), los_distribution.LoSDistribution(120, 120, 0, 20000),
+                                    los_distribution.LoSDistribution(45, 30, 0, 20000), los_distribution.LoSDistribution(8000, 3000, 0, 20000), los_distribution.LoSDistribution(5600, 1200, 0, 20000),
+                                    los_distribution.LoSDistribution(1200, 560, 0, 20000), los_distribution.LoSDistribution(500, 150, 0, 20000), los_distribution.LoSDistribution(5600, 1000, 0, 20000),
+                                    los_distribution.LoSDistribution(9500, 1000, 0, 20000), los_distribution.LoSDistribution(3600, 1200, 0, 20000),
+                                    los_distribution.LoSDistribution(120, 120, 0, 20000), los_distribution.LoSDistribution(45, 30, 0, 20000), los_distribution.LoSDistribution(3500, 300, 0, 20000),
+                                    los_distribution.LoSDistribution(4500, 3000, 0, 20000), los_distribution.LoSDistribution(1200, 300, 0, 20000), los_distribution.LoSDistribution(15000, 10000, 0, 20000),
+                                    los_distribution.LoSDistribution(4500, 1500, 0, 20000), los_distribution.LoSDistribution(8900, 900, 0, 20000), los_distribution.LoSDistribution(300, 120, 0, 20000)]}
+
+    los_df = pd.DataFrame(los_data, columns=['patient_group', 'ward_name', 'los_probability'])
+
+    generate_patient_pathway_los(['A', 'B', 'C', 'D'], patient_group_df, pathway_df, los_df, num_patients, num_steps, filename)
+
+
+def save_test_data(df, path):
+
+    df.to_pickle(path)
+
+
+if __name__ == "__main__":
+
+    generate_test_data(filename='..\\sample_datasets\\sample_patient_pathway_los_data', num_patients=50000)
+
+
+
+
+
